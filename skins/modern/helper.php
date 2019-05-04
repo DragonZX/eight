ffe090b6817cd5e775568180968f2f14305a9e8e
DragonZX <DragonZX@yandex.ru>
2019-05-04 22:59:47 +0300

    #4 Update to FontAwesome 5

--- skins/modern/helper.php
+++ skins/modern/helper.php
@@ -7,14 +7,18 @@ $skin_styles="
     <link rel=\"stylesheet\" href=\"/skins/modern/css/general.css\" type=\"text/css\" />
     <link rel=\"stylesheet\" href=\"/skins/modern/css/si_print_vacation_claim.css\" type=\"text/css\" />
     <link rel=\"stylesheet\" href=\"/skins/modern/css/si_print_vacation_claim.css\" type=\"text/css\" />
-    <link rel=\"stylesheet\" href=\"/libs/FontAwesome/font-awesome.min.css\" type=\"text/css\" />
+    <link rel=\"STYLESHEET\" href=\"/inteface/fonts/fontawesome/all.min.css\" type=\"text/css\" />
+	<link rel=\"STYLESHEET\" href=\"/inteface/fonts/fontawesome/brands.min.css\" type=\"text/css\" />
+	<link rel=\"STYLESHEET\" href=\"/inteface/fonts/fontawesome/solid.min.css\" type=\"text/css\" />
 ";
 $newwin_styles="
     <link rel=\"STYLESHEET\" href=\"/skins/modern/css/newwin.css\" type=\"text/css\" />
 	<link rel=\"STYLESHEET\" href=\"/skins/modern/css/staff.css\" type=\"text/css\" />
 	<link rel=\"STYLESHEET\" href=\"/skins/modern/css/si_print_vacation_claim.css\" type=\"text/css\" />
 	<link rel=\"STYLESHEET\" href=\"/skins/modern/css/general.css\" type=\"text/css\" />
-	<link rel=\"STYLESHEET\" href=\"/libs/FontAwesome/font-awesome.min.css\" type=\"text/css\" />
+	<link rel=\"STYLESHEET\" href=\"/inteface/fonts/fontawesome/all.min.css\" type=\"text/css\" />
+	<link rel=\"STYLESHEET\" href=\"/inteface/fonts/fontawesome/brands.min.css\" type=\"text/css\" />
+	<link rel=\"STYLESHEET\" href=\"/inteface/fonts/fontawesome/solid.min.css\" type=\"text/css\" />
 ";
 $skin_js="
     <script type=\"text/javascript\" src=\"/js/jquery-3.3.1.min.js\"></script>
